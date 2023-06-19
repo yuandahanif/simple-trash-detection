9f0205ce167e6447fb8138df7073861ba7105c3e
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2023-06-19 20:31:00 +0700

    feat: :sparkles: update ui and model

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ function App() {
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const imgRef = useRef<HTMLImageElement | null>(null);
-  const trashDetectionIdRef = useRef<number | null>(null);
   const objectDetectionIdRef = useRef<number | null>(null);
 
   const [modelLoaded, setModelLoaded] = useState(false);
@@ -31,13 +30,17 @@ function App() {
 
   const [showImage, setShowImage] = useState(false);
 
+  const [socres, setScores] = useState<
+    { name: string; score: number; isCorrect: boolean }[]
+  >([]);
+
   const capture = useCallback(() => {
     if (webcamRef.current) {
       const imageSrc = webcamRef.current.getScreenshot();
 
       if (imgRef.current && imageSrc) {
         setShowImage(true);
-        imgRef.current.src = imageSrc;
+        // imgRef.current.src = imageSrc;
       }
     }
   }, [webcamRef]);
@@ -91,14 +94,7 @@ function App() {
     }
   }
 
-  function trashDetectionStop() {
-    if (trashDetectionIdRef.current) {
-      capture();
-      clearInterval(trashDetectionIdRef.current);
-    }
-  }
-
-  async function trashDetection() {
+  const trashDetection = useCallback(async () => {
     setShowImage(false);
     setDetectedName("");
     const img = document.getElementById("img") as HTMLImageElement;
@@ -108,16 +104,24 @@ function App() {
 
     console.log(predictions);
 
-    //Rerun prediction by timeout
-    trashDetectionIdRef.current = setTimeout(() => trashDetection(), 500);
-
     predictions?.forEach((p) => {
-      if (p.probability > 0.8 && p.className != "nothing") {
+      if (p.probability > 0.9 && p.className != "nothing") {
         setDetectedName(p.className);
-        trashDetectionStop();
+
+        // setScores((scores) => {
+        //   scores.push({
+        //     isCorrect: true,
+        //     name: p.className,
+        //     score: p.probability,
+        //   });
+
+        //   scores = scores.slice(-4, 3);
+
+        //   return scores;
+        // });
       }
     });
-  }
+  }, [model]);
 
   const stopObjectDetection = () => {
     if (objectDetectionIdRef.current) {
@@ -143,10 +147,14 @@ function App() {
         },
         obj.score < 0.6
       );
+
+      if (obj.class == "person" && obj.score > 0.6) {
+        trashDetection();
+      }
     });
 
     objectDetectionIdRef.current = setTimeout(() => objectDetection(), 500);
-  }, [cocoSsdModel, drawBox]);
+  }, [cocoSsdModel, drawBox, trashDetection]);
 
   useEffect(() => {
     tf.ready()
@@ -167,52 +175,91 @@ function App() {
   }, [modelLoaded, objectDetection]);
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4">
+    <div className="mx-auto flex h-screen max-w-screen-xl flex-col items-center justify-center gap-4 bg-[#F4EAFF]">
       <div
-        className={`relative bg-white`}
+        className={`relative`}
         style={{ width: videoWidth, height: videoHeight }}
       >
-        <Webcam
-          audio={false}
-          id="img"
-          className={`absolute w-[${videoWidth}px] h-[${videoHeight}px]`}
-          ref={webcamRef}
-          screenshotQuality={1}
-          screenshotFormat="image/jpeg"
-          videoConstraints={videoConstraints}
-          mirrored
-        />
+        <div
+          className={`relative overflow-hidden rounded-md border-[.6em] border-[#00DA7E] bg-[#00DA7E]`}
+          style={{ width: videoWidth, height: videoHeight }}
+        >
+          <Webcam
+            audio={false}
+            id="img"
+            className={`absolute rounded-sm w-[${videoWidth}px] h-[${videoHeight}px]`}
+            ref={webcamRef}
+            screenshotQuality={1}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+            mirrored
+          />
 
-        <canvas
-          ref={canvasRef}
-          className="absolute"
-          width={videoWidth}
-          height={videoHeight}
+          <canvas
+            ref={canvasRef}
+            className="absolute"
+            width={videoWidth}
+            height={videoHeight}
+          />
+
+          <img
+            alt="foto"
+            ref={imgRef}
+            className="absolute grayscale"
+            style={{
+              width: videoWidth - 50,
+              height: videoHeight - 50,
+              display: showImage ? "none" : "none",
+              translate: "25px 25px",
+            }}
+          />
+        </div>
+
+        <img
+          alt="logo"
+          src="/images/logo.png"
+          className="absolute top-0 -translate-x-1/3 -translate-y-1/3"
         />
 
         <img
-          alt="foto"
-          ref={imgRef}
-          className="absolute grayscale"
-          style={{
-            width: videoWidth - 50,
-            height: videoHeight - 50,
-            display: showImage ? "block" : "none",
-            translate: "25px 25px",
-          }}
+          alt="trash left"
+          src="/images/trash-left.png"
+          className="absolute bottom-0 -translate-x-1/2 translate-y-1/3"
         />
+        <img
+          alt="trash right"
+          src="/images/trash-right.png"
+          className="absolute bottom-0 right-0 translate-x-1/3 translate-y-1/3"
+        />
+
         {detectedName != "" && (
-          <div className="absolute bottom-10 left-1/2 -translate-x-1/2 text-center text-white">
+          <div className="absolute bottom-10 left-1/2 -translate-x-1/2 bg-green-400 text-center text-white">
             {detectedName}
             <br />
           </div>
         )}
       </div>
 
-      {modelLoaded && (
+      <div className="mt-8 flex flex-col items-center font-IndieFlower">
+        <h1 className="mb-3 text-4xl font-semibold text-green-400">Riwayat</h1>
+        <div style={{ width: videoWidth }} className="flex flex-col gap-2">
+          {socres.reverse().map((score) => (
+            <div
+              key={score.score}
+              className={`w-full rounded-md p-4 text-white ${
+                score.isCorrect ? "bg-green-400" : "bg-red-400"
+              }`}
+            >
+              <span className="text-lg">{score.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* {modelLoaded && (
         <div className="flex gap-8">
           <button
-            className="rounded-full bg-red-300 px-5 py-2 text-white duration-200 hover:shadow-md"
+            className="rounded-full bg-red-300 px-5 py-2 font-IndieFlower text-white duration-200 hover:shadow-md"
             onClick={() => {
               trashDetection();
             }}
@@ -220,7 +267,7 @@ function App() {
             Mulai
           </button>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
---
+++ src/components/webcam.tsx
@@ -0,0 +1,42 @@
+import { forwardRef, useRef } from "react";
+import Webcam from "react-webcam";
+
+const videoWidth = 500;
+const videoHeight = 500;
+const videoConstraints = {
+  height: videoWidth,
+  width: videoHeight,
+  facingMode: "environment",
+};
+
+const CustomWebcam = forwardRef<Webcam>((props, ref) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  return (
+    <div
+      className={`relative bg-white`}
+      style={{ width: videoWidth, height: videoHeight }}
+    >
+      <Webcam
+        audio={false}
+        id="webcam"
+        className={`absolute w-[${videoWidth}px] h-[${videoHeight}px]`}
+        ref={ref}
+        screenshotQuality={1}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+        mirrored
+      />
+
+      <canvas
+        id="webcam-canvas"
+        ref={canvasRef}
+        className="absolute"
+        width={videoWidth}
+        height={videoHeight}
+      />
+    </div>
+  );
+});
+
+export default CustomWebcam;
