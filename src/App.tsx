826dd212ecb75cb80981748d6f3dc0ed1748ab5f
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2023-07-06 16:49:12 +0700

    feat: :sparkles: improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,306 +1,55 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState, useRef, useCallback } from "react";
-import * as tf from "@tensorflow/tfjs";
-import * as tmImage from "@teachablemachine/image";
-import Webcam from "react-webcam";
-
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
-
-const videoWidth = 500;
-const videoHeight = 500;
-const videoConstraints = {
-  height: videoWidth,
-  width: videoHeight,
-  facingMode: "environment",
-};
-
-const URL = "/model/trash/";
-const modelURL = URL + "model.json";
-const metadataURL = URL + "metadata.json";
+import Detection from "./views/detection";
+import Credit from "./views/credit";
+import Idle from "./views/idle";
 
 function App() {
-  const mascotContainerRef = useRef<HTMLDivElement>(null);
-  const webcamRef = useRef<Webcam>(null);
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const imgRef = useRef<HTMLImageElement | null>(null);
-  const objectDetectionIdRef = useRef<number | null>(null);
-
-  const [modelLoaded, setModelLoaded] = useState(false);
-  const [model, setModel] = useState<tmImage.CustomMobileNet>();
-  const [cocoSsdModel, setCocoSsdModel] = useState<cocoSsd.ObjectDetection>();
-  const [detectedName, setDetectedName] = useState("");
-
-  const [detectionStep, setDetectionStep] = useState<
-    "person" | "trash" | "bin" | "result"
-  >("person");
-
-  const [showImage, setShowImage] = useState(false);
-
-  const drawBox = useCallback(
-    (
-      {
-        x,
-        y,
-        w,
-        h,
-        text,
-      }: {
-        text: string;
-        x: number;
-        y: number;
-        w: number;
-        h: number;
-      },
-      clear?: boolean
-    ) => {
-      const ctx = canvasRef.current?.getContext("2d");
-
-      if (!ctx) return;
-
-      ctx.clearRect(0, 0, videoWidth, videoHeight);
-      if (clear) return;
-
-      ctx.strokeStyle = "red";
-      ctx.lineWidth = 2;
-      ctx.strokeRect(videoWidth - x - w, y, w, h);
-
-      ctx.fillStyle = "red";
-      ctx.font = "14px Arial";
-      ctx.textAlign = "center";
-      ctx.fillText(text, videoWidth - x - w + w * 0.5, y - 14);
-    },
-    []
+  const [component, setComponent] = useState<"detection" | "credit" | "idle">(
+    "idle"
   );
 
-  const displayMascot = () => {
-    if (mascotContainerRef.current == null) return;
-    const springTransform = [
-      { opacity: "1" },
-      { transform: "scale(.9)" },
-      { transform: "scale(1.1)" },
-      { transform: "scale(1)" },
-    ];
-
-    // mascotContainerRef.current.style.opacity = "0";
-    mascotContainerRef.current.classList.replace("hidden", "flex");
-    mascotContainerRef.current?.animate(springTransform, {
-      duration: 1000,
-      iterations: 1,
-      endDelay: 2000,
-    });
-
-    setTimeout(() => {
-      if (mascotContainerRef.current == null) return;
-      mascotContainerRef.current.classList.replace("flex", "hidden");
-    }, 3000);
-  };
-
-  async function loadModel() {
-    try {
-      const model = await tmImage.load(modelURL, metadataURL);
-      const coco = await cocoSsd.load();
-      setModel(model);
-      setCocoSsdModel(coco);
-      console.log("set loaded Model");
-    } catch (err) {
-      console.log(err);
-      console.log("failed load model");
-    }
-  }
-
-  const trashDetection = useCallback(async () => {
-    setShowImage(false);
-    setDetectedName("");
-    const img = document.getElementById("img") as HTMLImageElement;
-
-    //Start prediction
-    const predictions = await model?.predictTopK(img, 3);
-
-    console.log(predictions);
-
-    predictions?.forEach((p) => {
-      if (p.probability > 0.9 && p.className != "nothing") {
-        setDetectedName(p.className);
-        setDetectionStep("bin");
-      }
-    });
-  }, [model]);
-
-  const objectDetection = useCallback(async () => {
-    const img = document.getElementById("img") as HTMLImageElement;
-
-    const prediction = await cocoSsdModel?.detect(img);
-
-    console.log(prediction);
-
-    prediction?.forEach((obj) => {
-      drawBox(
-        {
-          x: obj.bbox[0],
-          y: obj.bbox[1],
-          w: obj.bbox[2],
-          h: obj.bbox[3],
-          text: obj.class,
-        },
-        obj.score < 0.6
-      );
-
-      if (obj.class == "person" && obj.score > 0.6) {
-        setDetectionStep("trash");
-      }
-    });
-  }, [cocoSsdModel, drawBox]);
-
-  const stoptDetection = () => {
-    if (objectDetectionIdRef.current) {
-      clearInterval(objectDetectionIdRef.current);
-    }
-  };
-
-  const detection = useCallback(() => {
-    console.log(
-      "file: App.tsx:170 ~ detection ~ detectionStep:",
-      detectionStep
-    );
-    if (detectionStep == "person") {
-      objectDetection();
-    }
-
-    if (detectionStep == "trash") {
-      trashDetection();
-    }
-
-    if (detectionStep == "bin") {
-      setDetectionStep("result");
-      // * SIMULASI deteksi sensor benar
-      // const clickListener = () => {
-      //   if (confirm("benar")) {
-      //     window.removeEventListener("click", clickListener);
-      //   }
-      // };
-      // window.addEventListener("click", clickListener);
-      // // * SIMULASI deteksi sensor salah
-      // const rightClickListener = (e: MouseEvent) => {
-      //   e.preventDefault();
-      //   if (confirm("benar")) {
-      //     if (confirm("salah")) {
-      //       setDetectionStep("person");
-      //       window.removeEventListener("contextmenu", rightClickListener);
-      //     }
-      //   }
-      //   return false;
-      // };
-      // window.addEventListener("contextmenu", rightClickListener);
-    }
-
-    if (detectionStep == "result") {
-      displayMascot();
-      setDetectionStep("person");
-    }
-
-    objectDetectionIdRef.current = setTimeout(() => detection(), 1000);
-  }, [detectionStep, objectDetection, trashDetection]);
-
   useEffect(() => {
-    tf.ready()
-      .then(() => {
-        return loadModel();
-      })
-      .finally(() => {
-        setModelLoaded(true);
-      });
+    // detect if user press spacebar and toggle component
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === " ") {
+        setComponent((prev) => (prev === "idle" ? "detection" : "idle"));
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
   }, []);
 
-  useEffect(() => {
-    if (modelLoaded) {
-      detection();
-    }
-
-    return () => stoptDetection();
-  }, [detection, modelLoaded, objectDetection]);
-
   return (
-    <div className="mx-auto flex h-screen max-w-screen-xl flex-col items-center justify-center gap-4 bg-[#F4EAFF]">
-      <div
-        className={`relative`}
-        style={{ width: videoWidth, height: videoHeight }}
-      >
-        <div
-          className={`relative overflow-hidden rounded-md border-[.6em] border-[#00DA7E] bg-[#00DA7E]`}
-          style={{ width: videoWidth, height: videoHeight }}
-        >
-          <Webcam
-            audio={false}
-            id="img"
-            className={`absolute rounded-sm w-[${videoWidth}px] h-[${videoHeight}px]`}
-            ref={webcamRef}
-            screenshotQuality={1}
-            screenshotFormat="image/jpeg"
-            videoConstraints={videoConstraints}
-            mirrored
+    <div className="relative flex min-h-screen w-full flex-col bg-[#F4EAFF]">
+      <div>
+        {component == "idle" && <Idle />}
+        {component == "detection" && (
+          <Detection
+            onStepEnd={() =>
+              setTimeout(() => {
+                setComponent("idle");
+              }, 5000)
+            }
           />
-
-          <canvas
-            ref={canvasRef}
-            className="absolute"
-            width={videoWidth}
-            height={videoHeight}
-          />
-
-          <img
-            alt="foto"
-            ref={imgRef}
-            className="absolute grayscale"
-            style={{
-              width: videoWidth - 50,
-              height: videoHeight - 50,
-              display: showImage ? "none" : "none",
-              translate: "25px 25px",
-            }}
-          />
-        </div>
-
-        <img
-          alt="logo"
-          src="/images/logo.png"
-          className="absolute top-0 -translate-x-1/3 -translate-y-1/3"
-        />
-
-        <img
-          alt="trash left"
-          src="/images/trash-left.png"
-          className="absolute bottom-0 -translate-x-1/2 translate-y-1/3"
-        />
-        <img
-          alt="trash right"
-          src="/images/trash-right.png"
-          className="absolute bottom-0 right-0 translate-x-1/3 translate-y-1/3"
-        />
-
-        {detectedName != "" && (
-          <div className="absolute bottom-10 left-1/2 -translate-x-1/2 rounded-md bg-green-400 px-3 py-1 text-center text-white">
-            {detectedName}
-          </div>
         )}
+        {component == "credit" && <Credit />}
       </div>
 
-      <div className="mt-8">
-        <div className="rounded-md bg-green-400 px-3 py-1 text-center text-xl text-white">
-          {detectionStep == "person" &&
-            "Perlihatkan wajah anda kedalam kamera!"}
-          {detectionStep == "trash" &&
-            "Wajah terdeteksi! Perlihatkan sampah ke kamera!"}
-          {detectionStep == "bin" &&
-            "Sampah terdeteksi! Silahkan pilih tempat sampah"}
-        </div>
-      </div>
+      <img
+        src="/images/bubble-gum-man-throws-the-letter-into-the-trash.png"
+        alt="trash-1"
+        className="fixed bottom-4 left-0 h-auto w-96 -translate-x-10"
+      />
+      <img
+        src="/images/bubble-gum-success.png"
+        alt="trash-2"
+        className="fixed bottom-4 right-0 h-auto w-96 -translate-x-10 rotate-45"
+      />
 
-      <div
-        ref={mascotContainerRef}
-        className="fixed hidden h-96 w-96 flex-col items-center justify-center gap-3 rounded-lg bg-green-400 text-white shadow-xl"
-      >
-        <img src="/images/mascot-happy.png" className="animate-bounce" />
-        <span className="text-2xl font-semibold">100 Buat kamu</span>
+      <div className="mx-auto">
+        Ilustarsi dan asset yang{" "}
+        <button onClick={() => setComponent("credit")}>digunakan</button>
       </div>
     </div>
   );
---
+++ src/views/credit.tsx
@@ -0,0 +1,24 @@
+const Credit = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          Illustration by{" "}
+          <a href="https://icons8.com/illustrations/author/eEbrZFlkyZbD">
+            Anna A
+          </a>{" "}
+          from <a href="https://icons8.com/illustrations">Ouch!</a>
+        </li>
+        <li>
+          Illustration by{" "}
+          <a href="https://icons8.com/illustrations/author/7WmtYU90j36d">
+            Fruzka
+          </a>{" "}
+          from <a href="https://icons8.com/illustrations">Ouch!</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Credit;
